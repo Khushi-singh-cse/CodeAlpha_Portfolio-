0c76228f3dcb8aed2ce3bd119ae95ac9e3d5ba01
Khushi-singh-cse <khushi.singh.cse@gmail.com>
2026-07-03 02:18:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-// Mobile Menu
-
-const menuToggle = document.querySelector(".menu-toggle");
-const navMenu = document.querySelector(".nav-links");
-
-menuToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-});// ==============================
+// ==============================
 // Typing Animation
 // ==============================
 
